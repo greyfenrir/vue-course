74ded4a8894329af01f74f8dd4eed2b62fc1c296
greyfenrir <grey.fenrir@gmail.com>
2022-08-15 16:04:07 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,46 @@
 <template>
+<div id="app">
+  <h1> todo it</h1>
+  <AddTodo
+    @add-todo="addTodo"
+  />
+  <TodoList 
+    v-bind:todos="todos"
+    @remove-todo="removeTodo"
+  />
+</div>
 
   
 </template>
 
 <script>
-
+import TodoList from '@/components/TodoList'
+import AddTodo from '@/components/AddTodo'
 export default {
   name: 'App',
-  components: {
+  data () {
+    return {
+      todos: []
+    }
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(json => {this.todos = json})
 
-  }
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter(t => t.id != id)
+    },
+    addTodo(todo) {
+      this.todos.push(todo)
+    }
+  },
+  components: {
+    TodoList,
+    AddTodo
+}
 }
 </script>
 
